536a3f5600647c66f767d33565f00b1f278878db
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-04 10:33:05 +0200

    edit cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,7 +7,7 @@ function Cart () {
             Développeur web en pleine expension,
             je me lance dans mon premier projet React <img alt="logo-react" style={{ height: '1.5%', width: '1.5%', backgroundColor: 'white', borderRadius: '2px'}}
                  src={logoreact}/> ! <br/>
-            Ce projet a pour but de présenter mes autres projets dans différents langages.
+            Ce projet a pour but de présenter mes autres projets dans différents langages. <strong>(en cours)</strong>
         </p>
     </div>)
 }
